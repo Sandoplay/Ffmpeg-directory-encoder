b8776886aa44dfa8bca6f81b61976c1ef7010fd8
Sandoplay <olesgta28@gmail.com>
2021-08-21 13:42:17 +0300

    Moving original files that converted to another path

--- Vp9Convert.py
+++ Vp9Convert.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import pathlib
 import subprocess
 from os import path
@@ -8,17 +9,21 @@ print(startPath)
 os.system("echo hello world")
 fileList = os.listdir()
 finalPath = path.join('Converted_Videos')
+origVidPath = path.join('Original_Videos_Converted')
 conVidPath = str(startPath) + "\\" + finalPath + "\\"
 print(conVidPath)
 if not os.path.isdir(finalPath):
     os.makedirs(finalPath)
+if not os.path.isdir(origVidPath):
+    os.makedirs((origVidPath))
 for i in fileList:
-    filext = os.path.splitext(i)[1]
-    if (filext == '.mp4') or filext == '.mov' or filext == '.MOV':
+    fileext = os.path.splitext(i)[1]
+    if (fileext == '.mp4') or fileext == '.mov' or fileext == '.MOV':
         n = i.split(".")
         # print(n[0])
         print(subprocess.run(
             "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -threads 12 -row-mt 1 -an -f null NUL && ^"
             "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -threads 12 -row-mt 1 -c:a libopus %s%s_vp9.webm" % (
                 i, i, conVidPath, n[0]), shell=True))
+        shutil.move(str(startPath) + "\\" + i, str(startPath) + "\\" + origVidPath + "\\" + i)
 input()
