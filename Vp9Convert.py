b66d19ab795b1f52a78757a9c12dfe562e55658c
Sandoplay <59182509+Sandoplay@users.noreply.github.com>
2022-05-04 16:36:11 +0300

    Add files via upload

--- Vp9Convert.py
+++ Vp9Convert.py
@@ -6,7 +6,7 @@ from os import path
 
 startPath = pathlib.Path().resolve()
 print(startPath)
-os.system("echo hello world")
+#os.system("echo hello world")
 fileList = os.listdir()
 finalPath = path.join('Converted_Videos')
 origVidPath = path.join('Original_Videos_Converted')
@@ -28,6 +28,6 @@ for i in fileList:
         print(subprocess.run(
             "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -threads 12 -row-mt 1 -an -f null NUL && ^"
             "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -threads 12 -row-mt 1 -c:a libopus %s%s_vp9.mp4" % (
-                i, i, conVidPath, n[0]), shell=True))
+                i, i, conVidPath, n[0]), shell=False))
         shutil.move(str(startPath) + "\\" + newName, str(startPath) + "\\" + origVidPath + "\\" + newName)
 input()
