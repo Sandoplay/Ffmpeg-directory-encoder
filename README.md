2a81546e5ae12458ff05feda0c1a8656fddee27c
Sandoplay <olesgta28@gmail.com>
2024-04-24 20:21:42 +0300

    Update to h265 and added metadata transfer to output video.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Ffmpeg-directory-encoder
-Simple script which convert any video in the folder to vp9 codec for video compression.
+Simple script which convert any video in the folder to h265 codec for video compression.
 To use it you need Ffmpeg installed and included in path.
--- Vp9Convert.py
+++ Vp9Convert.py
@@ -6,7 +6,6 @@ from os import path
 
 startPath = pathlib.Path().resolve()
 print(startPath)
-#os.system("echo hello world")
 fileList = os.listdir()
 finalPath = path.join('Converted_Videos')
 origVidPath = path.join('Original_Videos_Converted')
@@ -19,15 +18,17 @@ if not os.path.isdir(origVidPath):
     os.makedirs((origVidPath))
 for i in fileList:
     fileext = os.path.splitext(i)[1]
-    if (fileext == '.mp4') or fileext == '.mov' or fileext == '.MOV':
+    if ((fileext == '.mp4') or fileext == '.mkv' or fileext == '.MOV' or fileext == '.webm' or fileext == '.MP4'
+            or fileext == '.m4v'):
         oldName = i
-        n = i.split(".")
-        newName = i.replace(" ", "")
+        n= i.rsplit(".", 1)
+        newName = i.replace(" ", "_")
         os.rename(startPath+"\\"+oldName, startPath+"\\"+newName)
         # print(n[0])
         print(subprocess.run(
-            "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -threads 12 -row-mt 1 -an -f null NUL && ^"
-            "ffmpeg -i %s -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -threads 12 -row-mt 1 -c:a libopus %s%s_vp9.mp4" % (
-                i, i, conVidPath, n[0]), shell=False))
+            #"ffmpeg -i %s -c:v libx265 -crf 28 -preset medium -color_primaries 1 -color_trc 1 -colorspace 1 -movflags +write_colr -c:a aac -b:a 128k %s%s_vp9.mp4"
+            #"ffmpeg -y -i \"%s\" -c:v libx265 -crf 30 -preset medium -map 0 -map_metadata 0 -color_primaries 1 -color_trc 1 -colorspace 1 -movflags +write_colr -pass 1 -r 30 -an -f null NUL && ^"
+            "ffmpeg -i \"%s\" -c:v libx265 -crf 30 -preset fast -map 0:v -map 0:a:0 -map_metadata 0 -color_primaries 1 -color_trc 1 -colorspace 1 -movflags +write_colr -c:a aac -b:a 96k -r 30 \"%s%s%s_h265.mp4\""% (
+                newName, conVidPath, n[0], n[-1]), shell=True))
         shutil.move(str(startPath) + "\\" + newName, str(startPath) + "\\" + origVidPath + "\\" + newName)
 input()
